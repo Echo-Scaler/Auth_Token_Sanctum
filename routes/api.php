d56aaa8e2a4e06d0e150bee88788c5aa3c7fd5b6
Wai Yan <waiyan.msc.yu12@gmail.com>
2026-03-12 17:43:25 +0900

    create_route_groups

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\AuthController;
-
+use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/user', function (Request $request) {
@@ -12,6 +12,14 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);
 
+// Route::post('logout',[AuthController::class,'logout'])->middleware('auth:sanctum');
+// Route::get('me',[AuthController::class,'me'])->middleware('auth:sanctum');
+// Route::apiResource('notes',NoteController::class)->middleware('auth:sanctum');
 
-Route::post('logout',[AuthController::class,'logout'])->middleware('auth:sanctum');
-Route::get('me',[AuthController::class,'me'])->middleware('auth:sanctum');
+//change to groups
+Route::middleware('auth:sanctum')->group(function(){
+Route::post('logout',[AuthController::class,'logout']);
+Route::get('me',[AuthController::class,'me']);
+
+Route::apiResource('notes',NoteController::class);
+});
